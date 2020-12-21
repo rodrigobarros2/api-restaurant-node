e7eb268b58990db9d7e43418c643b972c1fa4b9c
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-12-21 16:38:46 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '177f157475c8bd31dee98b4826fc99fd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a112096d-d4b4-48b5-a473-71ff33b31960'
+};
